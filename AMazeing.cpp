3b07d3e8a8744b29c98ffd879cc8549847feba74
RChopin <hapchopin@gmail.com>
2017-05-14 16:11:48 +0200

    start

---
+++ AMazeing.cpp
@@ -0,0 +1,17 @@
+// AMazeing.cpp: Okreœla punkt wejœcia dla aplikacji konsoli.
+//
+
+#include "stdafx.h"
+#include "AMazeing.h"
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+
+	
+
+    return yolo;
+}
+
---
+++ AMazeing.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <iostream>
+using namespace std;
+void yolo() {
+	cout << "yo mama is shit" << endl;
+}

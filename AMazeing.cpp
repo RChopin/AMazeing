3c9ec60e14f1ed77adb35c3f3a1674aa6f5153d5
RChopin <hapchopin@gmail.com>
2017-05-19 22:29:09 +0200

    Fight added
    You can fight that harsh spider. You almost can't lose.

--- AMazeing.cpp
+++ AMazeing.cpp
@@ -16,7 +16,7 @@ void EndThis() {
 
 int main()
 {
-	
+	/*
 	srand(time(NULL));
 	float a = rand() % 10 + 1;
 
@@ -27,13 +27,74 @@ int main()
 
 
 
-	cout << PlayerHealth << "hp" << endl;
+	cout << PlayerHealth << " hp" << endl;
 	system("pause");
 
 	e1->attack(3);
 
-	cout << PlayerHealth << "hp" << endl;
+	cout << PlayerHealth << " hp" << endl;
+	*/
+	string EnemyName = "Capitan Spider";
+	cout << "You have met " << EnemyName << endl << "You can attack it by typing 1 or 2" << endl << "1 - Sword (27-45 dmg)" << endl << "2 - SpiderKiller 1 time use (97 dmg)" << endl << "Or you can run by typing 3 (loosing 20% of power)" << endl;
+	bool canUseSpiderKiller = 1;
+	int Activity1;
+	
+	while (EnemyHealth > 0, PlayerHealth > 0) {
+		cout << "Chose your weapon" << endl;
+		cin >> Activity1;
+		if (Activity1 == 1) {
+			srand(time(NULL));
+			double a = rand() % 27 + 18;
 
+			Player att;
+			Player *p1 = &att;
+
+			p1->attack(a);
+			
+			canBeAttacked = 1;
+
+			cout << PlayerHealth << " hp, you have" << endl;
+			system("pause");
+		}  
+		if (Activity1 == 2 && canUseSpiderKiller == 1) {
+			double b = 97;
+
+			Player att;
+			Player *p1 = &att;
+
+			p1->attack(b);
+			canUseSpiderKiller = 0;
+			canBeAttacked = 1;
+
+
+			cout << PlayerHealth << " hp, you have" << endl;
+			system("pause");
+
+		}  
+		if (Activity1 == 2 && canUseSpiderKiller == 0) {
+			cout << "You dont have SpiderKiller anymore" << endl;
+		}
+		if (Activity1 == 3) {
+			cout << "You are fearful idiot" << endl << "You have to fight spider anyway" << endl;
+			PlayerPower = PlayerPower * 0.8;
+			canBeAttacked = 0;
+			system("pause");
+			
+		}
+		if (EnemyHealth <= 0) { cout << "You have deafeten " << EnemyName << endl; }
+		if (EnemyHealth > 0 && PlayerHealth > 0 && canBeAttacked == 1) {
+			cout << "Now " << EnemyName << " attacks you!" << endl;
+			srand(time(NULL));
+			double c = rand() % 30 + 1;
+
+			Spider s;
+			Enemy *e1 = &s;
+
+			e1->attack(c);
+
+		}
+		if (PlayerHealth <= 0) { cout << "YOU HAVE DIED!" << endl; }
+	};
 
 
 	EndThis();
--- AMazeing.h
+++ AMazeing.h
@@ -6,26 +6,34 @@ using namespace std;
 
 bool canAttack = 1;
 bool canBeAttacked = 1; 
-float PlayerHealth = 100;
+double PlayerHealth = 100;
+double EnemyHealth = 100;
+double PlayerPower = 1;
+string EnemyName = "Capitan Spider";
 
 
 
 class Enemy {
 public:
-	virtual void attack(float AttackDmg) {}
-
-	//string EnemyName;
-
-	
+	virtual void attack(double AttackDmg) {}
 };
 
 class Spider : public Enemy {
 public:
-	string EnemyName;
-	void attack(float AttackDmg) {
-		if (PlayerHealth >= AttackDmg) { PlayerHealth -= AttackDmg; };
+	void attack(double AttackDmg) {
+		if (PlayerHealth > 0) { PlayerHealth -= AttackDmg; };
 		cout << EnemyName << " attacked you, causing " << AttackDmg << " damage" << endl;
 		cout << "You have " << PlayerHealth << " hp left" << endl;
+
+	}
+};
+
+class Player : public Enemy {
+public:
+	void attack(double AttackDmg) {
+		if (EnemyHealth > 0) { EnemyHealth -= (AttackDmg * PlayerPower); };
+		cout << "You have attacked "<< EnemyName << ", causing " << AttackDmg*PlayerPower << " damage" << endl;
+		cout << EnemyName << " have " << EnemyHealth << " hp left" << endl;
 
 	}
 };

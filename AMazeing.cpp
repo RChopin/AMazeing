606682af017ce775f94c39d5807a43d1538ffbad
RChopin <hapchopin@gmail.com>
2017-05-19 20:30:12 +0200

    Working random attack system
    yay it works

--- AMazeing.cpp
+++ AMazeing.cpp
@@ -1,17 +1,41 @@
-// AMazeing.cpp: Okreœla punkt wejœcia dla aplikacji konsoli.
+// Radek (c) 2017
 //
 
 #include "stdafx.h"
 #include "AMazeing.h"
 #include <iostream>
+#include <ctime>
 using namespace std;
 
+void EndThis() {
+	//Closing code
+	cout << "type any value to end app..." << endl;
+	string EndThisShit;
+	cin >> EndThisShit;
+}
 
 int main()
 {
-
 	
+	srand(time(NULL));
+	float a = rand() % 10 + 1;
 
-    return 0;
-}
+	Spider s;
+	Enemy *e1 = &s;
 
+	e1->attack(a);
+
+
+
+	cout << PlayerHealth << "hp" << endl;
+	system("pause");
+
+	e1->attack(3);
+
+	cout << PlayerHealth << "hp" << endl;
+
+
+
+	EndThis();
+	return 0;
+};
--- AMazeing.h
+++ AMazeing.h
@@ -1,9 +1,31 @@
 #pragma once
 #include <iostream>
+#include <string>
 using namespace std;
+
+
+bool canAttack = 1;
+bool canBeAttacked = 1; 
+float PlayerHealth = 100;
+
 
 
 class Enemy {
 public:
-	virtual void attack(){}
+	virtual void attack(float AttackDmg) {}
+
+	//string EnemyName;
+
+	
+};
+
+class Spider : public Enemy {
+public:
+	string EnemyName;
+	void attack(float AttackDmg) {
+		if (PlayerHealth >= AttackDmg) { PlayerHealth -= AttackDmg; };
+		cout << EnemyName << " attacked you, causing " << AttackDmg << " damage" << endl;
+		cout << "You have " << PlayerHealth << " hp left" << endl;
+
+	}
 };
